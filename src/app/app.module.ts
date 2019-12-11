3cce9463f4ea443390add6f1fb0bf7e24dfec246
Chen.Weimi <737149646@qq.com>
2019-12-11 17:03:55 +0800

    5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app/events-app.component';
+import { EventsListComponent } from './events-list/events-list.component';
+import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
+import { NavComponent } from './nav/nav.component';
+import { EventService } from './shared/event.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
